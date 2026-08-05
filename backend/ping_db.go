dc036a9405d4a252ea21028d99a7d33aed4a8981
Ifeanyi Felix <ifeanyireed@gmail.com>
2026-08-05 11:32:30 +0100

    fixed: unauthenticated user login

---
+++ backend/ping_db.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"fmt"
+	"checkin-backend/config"
+)
+func main() {
+	db := config.InitDB()
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Ping(); err != nil {
+		fmt.Println("DB is DOWN:", err)
+	} else {
+		fmt.Println("DB is UP and responsive!")
+	}
+}
